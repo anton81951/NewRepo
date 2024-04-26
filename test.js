215f2c043bd773b9513aa88f568a555c44cf5675
Anton81951 <antonbugaichuk@gmail.com>
2024-04-26 16:16:32 +0300

    1

--- test.js
+++
@@ -1,5 +0,0 @@
-function toCamelCase(str) {
-    const cleanedStr = str.replace(/[_-]/g, '');
-    const words = cleanedStr.split(/\s+/).map(word => word.charAt(0).toUpperCase() + word.slice(1));
-    return words.join("");
-}
